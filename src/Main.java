700aed92f48b71d7d03ca3ffa39eb68bce301047
ngoanhkhoitq2024-del <ngoanhkhoitq2024@gmail.com>
2026-06-29 01:47:37 +0700

    Update Session_3_b1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("===== NHẬP THÔNG TIN HÓA ĐƠN =====");
         System.out.print("Tên khách hàng: ");
         String customerName = scanner.nextLine();
         System.out.print("Tên sản phẩm: ");
@@ -19,13 +20,15 @@ public class Main {
         double moneyVAT = total * 0.08;
         double payment = (total - discount + moneyVAT);
 
-        System.out.printf("\nKhách hàng: %s\n", customerName);
+        System.out.println("\n===== HÓA ĐƠN =====");
+        System.out.printf("Khách hàng: %s\n", customerName);
         System.out.printf("Sản phẩm: %s\n", productName);
-        System.out.printf("Giá: %,.0f VNĐ\n", price);
+        System.out.printf("Giá: %,.2f VNĐ\n", price);
         System.out.printf("Số lượng: %s\n", quantity);
-        System.out.printf("Thành tiền: %,.0f\n", total);
-        System.out.printf("Giảm giá: %,.0f\n", discount);
-        System.out.printf("Tiền VAT: %,.0f\n", moneyVAT);
-        System.out.printf("Tổng tiền thanh toán: %,.0f VNĐ\n", payment);
+        System.out.printf("Thành tiền: %,.2f VNĐ\n", total);
+        System.out.printf("Giảm giá thành viên (10%%): %,.2f VNĐ\n", discount);
+        System.out.printf("Tiền VAT (8%%): %,.2f VNĐ\n", moneyVAT);
+        System.out.printf("Tổng tiền thanh toán: %,.2f VNĐ\n", payment);
+        System.out.println("===================================");
     }
 }
